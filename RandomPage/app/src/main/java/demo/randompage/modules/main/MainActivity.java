80a54dd502ea45263ef7e095609353873ed4bdb2
smyyoukey <412132892@qq.com>
2018-02-26 09:52:25 +0800

    add list

--- RandomPage/app/src/main/java/demo/randompage/modules/main/MainActivity.java
+++ RandomPage/app/src/main/java/demo/randompage/modules/main/MainActivity.java
@@ -7,18 +7,30 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import demo.randompage.R;
+import demo.randompage.database.Item;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+public class MainActivity extends AppCompatActivity {
 
 
     private RecyclerView mRecyclerView;
-
+    private ArrayList mPicList;
+    private RVAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        mPicList =new ArrayList();
+//        List<Item> list = Arrays.asList(Item.ITEMS);
+//        mPicList.addAll(list);
         initView();
 
     }
@@ -27,12 +39,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         StaggeredGridLayoutManager mlayoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(mlayoutManager);
+        mAdapter = new RVAdapter(this,mPicList);
+        mRecyclerView.setAdapter(mAdapter);
 
     }
 
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-    }
 }
--- RandomPage/app/src/main/java/demo/randompage/modules/main/RVAdapter.java
+++ RandomPage/app/src/main/java/demo/randompage/modules/main/RVAdapter.java
@@ -1,16 +1,25 @@
 package demo.randompage.modules.main;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 
 import demo.randompage.R;
+import demo.randompage.database.Item;
 
 /**
  * Created by smy on 18-2-10.
@@ -24,16 +33,18 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
 
     public RVAdapter(Context context, ArrayList list) {
         this.mContext = context;
-        this.mArrayList = list;
+//        this.mArrayList = list;
 
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder{
-        public ImageView mPhoto;
-        public TextView mProvide;
+        ImageView mPhoto;
+         TextView mProvide;
+         View mPicView;
 
         public ViewHolder(View itemView) { //set view in this method
             super(itemView);
+            mPicView = itemView;
             mPhoto = (ImageView)itemView.findViewById(R.id.imageview_item);
             mProvide = (TextView)itemView.findViewById(R.id.textview_name);
         }
@@ -46,17 +57,63 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder> {
         final RVAdapter.ViewHolder holder = new RVAdapter.ViewHolder(view);
 
 
-        return null;
+        return holder;
+    }
+   private Item mItem;
+    @Override
+    public void onBindViewHolder(final RVAdapter.ViewHolder holder, final int position) {
+        mItem = getItem(position);
+        Log.d("item","item's URL : "+mItem.getThumbnailUrl());
+        holder.mPhoto = (ImageView)holder.mPicView.findViewById(R.id.imageview_item);
+        Picasso.with(holder.mPhoto.getContext())
+                .load(mItem.getThumbnailUrl())
+                .into(holder.mPhoto);
+
+        holder.mProvide.setText(mItem.getName());
+
+        holder.mPicView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int p = holder.getAdapterPosition();
+                mItem = getItem(p);
+
+                // Construct an Intent as normal
+                Intent intent = new Intent(mContext, DetailActivity.class);
+                intent.putExtra(DetailActivity.EXTRA_PARAM_ID, mItem.getId());
+                // BEGIN_INCLUDE(start_activity)
+                /**
+                 * Now create an {@link android.app.ActivityOptions} instance using the
+                 * {@link ActivityOptionsCompat#makeSceneTransitionAnimation(Activity, Pair[])} factory
+                 * method.
+                 */
+                ActivityOptionsCompat activityOptions = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        (Activity) mContext,
+                        // Now we provide a list of Pair items which contain the view we can transitioning
+                        // from, and the name of the view it is transitioning to, in the launched activity
+                        new Pair<View, String>(v.findViewById(R.id.imageview_item),
+                                DetailActivity.VIEW_NAME_HEADER_IMAGE),
+                        new Pair<View, String>(v.findViewById(R.id.textview_name),
+                                DetailActivity.VIEW_NAME_HEADER_TITLE)
+                );
+
+                // Now we can start the Activity, providing the activity options as a bundle
+                ActivityCompat.startActivity(mContext, intent, activityOptions.toBundle());
+                // END_INCLUDE(start_activity)
+            }
+        });
     }
 
     @Override
-    public void onBindViewHolder(RVAdapter.ViewHolder holder, final int position) {
-
-
-}
+    public int getItemCount() {
+        return Item.ITEMS.length;
+    }
 
     @Override
-    public int getItemCount() {
-        return 0;
+    public long getItemId(int position) {
+        return super.getItemId(position);
+
+    }
+    public Item getItem(int position){
+        return Item.ITEMS[position];
     }
 }
